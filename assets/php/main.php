9975db7d750795305bb166c298abb67a30afb6e0
Hediwf3 <32874490+Hediwf3@users.noreply.github.com>
2018-01-12 12:51:32 +0100

    s

--- assets/php/main.php
+++ assets/php/main.php
@@ -155,6 +155,7 @@
     $('.pop-up-button').click(function () {
         $('.wrapper').toggleClass('show');
         $('.wrapper-two').removeClass('show');
+        
     });
     $('.pop-up-button-sign-in').click(function () {
         $('.wrapper-two').toggleClass('show');
